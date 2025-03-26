beec1b20289fee14f60576b3acf341ce1d9ba07a
visaj-sst <visaj@sevensquaretech.com>
2025-03-26 14:23:36 +0530

    added "_id" in the header

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const corsOptions = {
 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization", "ids"],
+  allowedHeaders: ["Content-Type", "Authorization", "ids", "_id"],
 };
 
 app.use(cors(corsOptions));
